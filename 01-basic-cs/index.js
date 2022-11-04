39bdc5f9e3dcd71e7c22ebd345c484f759fd6d15
fcode <46227045+franciscoguevara03@users.noreply.github.com>
2022-11-04 18:57:40 -0400

    Code from Francisco Guevara

--- 01-basic-cs/index.js
+++ 01-basic-cs/index.js
@@ -5,7 +5,37 @@ const assert = require('assert')
 
 const database = require('./database.json')
 
-const total = 0 // TODO
+var sold = {}
+var soldHats = 0
+var maxSolds = 0
+
+//Funcion que determina los 3 Sombreros mas vendidos
+function top3(numbers) {  
+  numbers = numbers.sort((a,b) => b - a);
+  return [numbers[0], numbers[1], numbers[2]]
+}
+
+//Bucle que permite ordenar e incluye en un array nuevo los sobreros vendidos juntos a las cantidades de los mismos segun su id
+Object.entries(database).forEach(([key,value]) => {
+  value.hats.forEach(function(numero){
+    sold[numero.id] = (sold[numero.id] || 0) + 1
+  })
+})
+
+soldHats =  Object.values(sold)
+
+//Bucle que permite escribir en consola por Clave Valor los id's de los Sombreros vendidos junto a las cantidades vendidas de los mismos
+Object.entries(sold).forEach(([key,value]) => {
+  console.log("Hat (" + key + ") sold " + value)
+})
+
+maxSolds = top3(soldHats)    // Calculamos los 3 Sombreros mas vendidos y los insertamos en un array
+
+const total = maxSolds.reduce((a, b) => a + b, 0) // TODO   // Sumamos los 3 Sombreros mas vendidos
+
+console.log("**************************Resultado de Ejecucion del Codigo ****************************")
+console.log("->" + maxSolds + " => " + total)
+
 
 // Throws error on failure
 assert.equal(total, 23, `Invalid result: ${total} != 23`)
